de25c337275d4e1bc1a473f196f00623d69198df
Damien Engels <engelsdamien@gmail.com>
2014-12-22 01:22:04 +0100

    Add support for a forest of inheritence classes

--- lib/CodeGen/VTProtect.cpp
+++ lib/CodeGen/VTProtect.cpp
@@ -42,52 +42,144 @@ namespace {
   class VTProtect : public ModulePass {
 
   private:
-    typedef std::map<llvm::Type*, llvm::Constant*> TableMapping;
+    typedef std::map<llvm::Type*, llvm::GlobalVariable*> TableMapping;
+    /* Mapping between each class and its VirtualTable */
     TableMapping VTables;
 
     typedef std::map<llvm::GlobalVariable*, unsigned> TablePosition;
+    /* Keeps track of the position of each old VTable in their corresponding new global one */
     TablePosition Indexes;
 
     typedef std::map<llvm::Type*, std::vector<llvm::Type*> > AdjacencyList;
+    /* Map from parents to their list of childs */
     AdjacencyList Hierarchy;
 
-    void inOrderTraversal(std::vector<llvm::Constant*>& initializers, std::vector<llvm::Type*>& types, llvm::Type* Root,
+    typedef std::map<llvm::Type*, llvm::Type*> RootMap;
+    /* Map from every node in the type tree to it's root */
+    RootMap ChildToRoots;
+
+    /* Map of each Root to it's corresponding new global table */
+    TableMapping RootTables;
+
+    typedef llvm::SmallPtrSet<llvm::Type*, 100> TypeSet;
+    TypeSet Roots;
+
+    unsigned NumTables = 0;
+
+    llvm::GlobalVariable* createGlobalTable(llvm::Module &M, std::vector<llvm::Constant*> Initializers, std::vector<llvm::Type*> Types, unsigned MaxTableLen){
+
+        llvm::StructType* GlobalVTTy = llvm::StructType::create(Types);
+        llvm::Constant* GlobalVTInit = llvm::ConstantStruct::get(GlobalVTTy, Initializers);
+
+        // Use unique name
+        char Name[128];
+        ::sprintf(Name, "SuperTable%d", NumTables++);
+        llvm::GlobalVariable* GlobalVT = dyn_cast<llvm::GlobalVariable>(M.getOrInsertGlobal(Name, GlobalVTTy));
+        GlobalVT->setInitializer(GlobalVTInit);
+        GlobalVT->setAlignment(MaxTableLen);
+
+        return GlobalVT;
+    }
+
+    void collectHierarchyMetadata(llvm::Module &M, llvm::StringRef Name){
+      llvm::NamedMDNode* HierarchyMetadata = M.getNamedMetadata(Name);
+
+      /* Tranform hierarchy tree to have it in a nicer format */
+      for(llvm::MDNode* op : HierarchyMetadata->operands()){
+        assert(op->getNumOperands() == 2);
+        llvm::Type* Child = op->getOperand(0)->getType();
+        llvm::Type* Parent = op->getOperand(1)->getType();
+
+        Hierarchy[Parent].push_back(Child);
+
+        while(ChildToRoots.find(Parent) != ChildToRoots.end()){
+          Parent = ChildToRoots[Parent];
+        }
+
+        for(auto& OldRootChild : ChildToRoots){
+          if(OldRootChild.second == Child){
+            OldRootChild.second = Parent;
+          }
+        }
+
+        ChildToRoots[Child] = Parent;
+
+      }
+
+      for(auto& ChildRoot : ChildToRoots){
+        Roots.insert(ChildRoot.second);
+      }
+
+    }
+
+    /* Returns the size of the largest vTable */
+    unsigned collectVTablesMetadata(llvm::Module &M, llvm::StringRef Name, DataLayout &DL){
+      llvm::NamedMDNode* VTablesMetadata = M.getNamedMetadata(Name);
+
+      unsigned MaxLen = 0;
+      for(llvm::MDNode* op : VTablesMetadata->operands()){
+        assert(op->getNumOperands() == 2);
+        llvm::Type* Typ = op->getOperand(0)->getType();
+        llvm::GlobalVariable* Table = dyn_cast<GlobalVariable>(op->getOperand(1));
+
+        VTables[Typ] = Table;
+        if(ChildToRoots.find(Typ) == ChildToRoots.end()){
+          ChildToRoots[Typ] = Typ;
+          Roots.insert(Typ);
+        }
+
+        unsigned Size = DL.getTypeSizeInBits(dyn_cast<PointerType>(Table->getType())->getElementType());
+        if(Size > MaxLen){
+          MaxLen = Size;
+        }
+      }
+
+      return MaxLen;
+    }
+
+    llvm::Type* getRightMostChild(llvm::Type* parent){
+        AdjacencyList::const_iterator Childrens = Hierarchy.find(parent);
+        if(Childrens == Hierarchy.end() || Childrens->second.empty()){
+            return parent;
+        } else {
+           return getRightMostChild(Childrens->second.back());
+        }
+    }
+
+    void collectInitializers(std::vector<llvm::Constant*>& initializers, std::vector<llvm::Type*>& types, llvm::Type* Root,
                           unsigned MaxLen, DataLayout &DL, LLVMContext& context){
 
-        llvm::GlobalVariable* Table = dyn_cast_or_null<GlobalVariable>(VTables[Root]);
-        if(Table){
-        llvm::Type* RealType = dyn_cast<PointerType>(Table->getType())->getElementType();
+      llvm::GlobalVariable* Table = dyn_cast_or_null<GlobalVariable>(VTables[Root]);
+      unsigned RealSize = 0;
 
-        // TODO: Find out what do to with table that have no initializer
-        //
+      if(Table){ //Not sure if table should always be defined
+        llvm::Type* RealType = dyn_cast<PointerType>(Table->getType())->getElementType();
+        RealSize = DL.getTypeSizeInBits(RealType);
 
         if(Table->hasInitializer()){
           Indexes[Table] = types.size();
+          // TODO: Find out what do to with table that have no initializer
           initializers.push_back(Table->getInitializer());
           types.push_back(RealType);
-
-          unsigned RealSize = DL.getTypeSizeInBits(RealType);
-          for (unsigned i = 0; i < (MaxLen-RealSize)/8; ++i){
-            llvm::Type* Char = llvm::Type::getInt8Ty(context);
-            initializers.push_back(llvm::Constant::getNullValue(Char));
-            types.push_back(Char);
-          }
         }
-        }else{
-          for (unsigned i = 0; i < MaxLen/8; ++i){
-            llvm::Type* Char = llvm::Type::getInt8Ty(context);
-            initializers.push_back(llvm::Constant::getNullValue(Char));
-            types.push_back(Char);
-          }
+      }
 
-        }
+      // Padding
+      llvm::Type* Char = llvm::Type::getInt8Ty(context);
+      unsigned CharSize = DL.getTypeSizeInBits(Char);
+      unsigned PaddingSize = MaxLen - RealSize;
+      assert(PaddingSize % CharSize == 0);
+      llvm::Type* ArrayTy = llvm::ArrayType::get(Char, PaddingSize/CharSize);
+      types.push_back(ArrayTy);
+      initializers.push_back(llvm::Constant::getNullValue(ArrayTy));
 
-        AdjacencyList::const_iterator Childrens = Hierarchy.find(Root);
-        if(Childrens != Hierarchy.end()){
-            for(const auto& Child: Childrens->second){
-                inOrderTraversal(initializers, types, Child, MaxLen, DL, context);
-            }
+      // Run on childrens
+      AdjacencyList::const_iterator Childrens = Hierarchy.find(Root);
+      if(Childrens != Hierarchy.end()){
+        for(const auto& Child: Childrens->second){
+          collectInitializers(initializers, types, Child, MaxLen, DL, context);
         }
+      }
     }
 
   public:
@@ -100,65 +192,96 @@ namespace {
       //             << M.getModuleIdentifier() << "\n";
 
       DataLayout DL(&M);
-      IRBuilder<true, llvm::ConstantFolder> IR(M.getContext());
+      IRBuilder<true, llvm::ConstantFolder> IRB(M.getContext());
 
-      llvm::NamedMDNode* HierarchyMetadata = M.getNamedMetadata("cps.hierarchy");
-      llvm::NamedMDNode* VTablesMetadata = M.getNamedMetadata("cps.vtables");
-      // TODO: Change this to handle forests (aka. multiple roots)
-      llvm::Type* Root = nullptr;
-      unsigned MaxLen = 0;
-
-
-      /* Tranform hierarchy tree to have it in a nicer format */
-      for(llvm::MDNode* op : HierarchyMetadata->operands()){
-          assert(op->getNumOperands() == 2);
-          llvm::Value* Child = op->getOperand(0);
-          llvm::Value* Parent = op->getOperand(1);
-
-          if(Root == nullptr || Child->getType() == Root){
-              Root = Parent->getType();
-          }
-
-          Hierarchy[Parent->getType()].push_back(Child->getType());
-      }
-
-
-      for(llvm::MDNode* op : VTablesMetadata->operands()){
-          assert(op->getNumOperands() == 2);
-          llvm::Value* Class = op->getOperand(0);
-          llvm::Constant* Table = dyn_cast<Constant>(op->getOperand(1));
-
-          VTables[Class->getType()] = Table;
-
-          //unsigned Size = DL.getPointerTypeSize(Table->getType());
-          unsigned Size = DL.getTypeSizeInBits(dyn_cast<PointerType>(Table->getType())->getElementType());
-          if(Size > MaxLen){
-              MaxLen = Size;
-          }
-      }
+      collectHierarchyMetadata(M, "cps.hierarchy");
+      unsigned MaxTableLen = collectVTablesMetadata(M, "cps.vtables", DL);
 
       // Align the length of the largest vtable to closest power of 2
-      MaxLen = 1 << ((int) ceil(log(MaxLen)/log(2)));
+      MaxTableLen = 1 << ((int) ceil(log(MaxTableLen)/log(2)));
 
-      std::vector<llvm::Constant*> Initializers;
-      std::vector<llvm::Type*> Types;
 
-      inOrderTraversal(Initializers, Types, Root, MaxLen, DL, M.getContext());
+      for(llvm::Type* Root : Roots){
+        std::vector<llvm::Constant*> Initializers;
+        std::vector<llvm::Type*> Types;
 
-      if(Types.empty()){
-          return false;
+        collectInitializers(Initializers, Types, Root, MaxTableLen, DL, M.getContext());
+
+        if(Types.empty()){
+          continue; // Can this really happen ?
+        }
+
+        llvm::GlobalVariable* GlobalVT = createGlobalTable(M, Initializers, Types, MaxTableLen);
+        RootTables[Root] = GlobalVT;
+
+        for(auto& ChildRoot: ChildToRoots){
+          if(ChildRoot.second == Root){
+            llvm::GlobalVariable *Table = VTables[ChildRoot.first];
+            //Check why table is sometimes null here
+            if(Table && Table->getNumUses()){
+                llvm::Value* newTablePtr = IRB.CreateConstGEP2_32(GlobalVT, 0, Indexes[Table]);
+                Table->replaceAllUsesWith(newTablePtr);
+            }
+          }
+        }
       }
 
-      llvm::StructType* GlobalVTTy = llvm::StructType::create(Types);
-      llvm::Constant* GlobalVTInit = llvm::ConstantStruct::get(GlobalVTTy, Initializers);
 
-      llvm::Constant* GlobalVT = M.getOrInsertGlobal("SuperTable", GlobalVTTy);
-      dyn_cast<llvm::GlobalVariable>(GlobalVT)->setInitializer(GlobalVTInit);
+      for(llvm::Function& fun: M.getFunctionList()){
+        if(fun.isIntrinsic()) continue; // Not too sure about this
+        llvm::Value* Abort = M.getOrInsertFunction("abort", Type::getVoidTy(M.getContext()), nullptr);
+        llvm::BasicBlock* ExitBB = BasicBlock::Create(M.getContext(), "exit", &fun);
 
-      for(auto& TblIndex : Indexes){
-          llvm::GlobalVariable *Table = TblIndex.first;
-          llvm::Value* newTablePtr = IR.CreateConstGEP2_32(GlobalVT,0, TblIndex.second);
-          Table->replaceAllUsesWith(newTablePtr);
+        ExitBB->getInstList().push_back(CallInst::Create(Abort));
+        llvm::Instruction* Terminator;
+        if(fun.getReturnType()->isVoidTy()){
+          Terminator = ReturnInst::Create(M.getContext());
+        }else{
+          Terminator = ReturnInst::Create(M.getContext(), llvm::UndefValue::get(fun.getReturnType()));
+        }
+        ExitBB->getInstList().push_back(Terminator);
+
+        for(llvm::BasicBlock& block : fun.getBasicBlockList()){
+          for(llvm::Instruction& inst : block.getInstList()){
+            llvm::MDNode* metadata = inst.getMetadata("cps.vload");
+            if(metadata){
+              // I don't think this is necessarily true
+              llvm::Instruction* vptr = inst.getNextNode();
+
+              llvm::Type* Typ = metadata->getOperand(0)->getType();
+              llvm::Type* End = getRightMostChild(Typ);
+              llvm::GlobalVariable* GlobalVT = RootTables[ChildToRoots[Typ]];
+
+              IRB.SetInsertPoint(&block, vptr);
+              llvm::Value* BasePtr = IRB.CreateConstGEP2_32(GlobalVT, 0, Indexes[VTables[Typ]]);
+              llvm::Value* EndPtr = IRB.CreateConstGEP2_32(GlobalVT, 0, Indexes[VTables[End]]);
+              llvm::Value* TblPtr = IRB.CreateConstGEP1_64(vptr->getOperand(0), -2); // Apparently there is need for fixing the pointer
+
+              llvm::BasicBlock* PrevBB = vptr->getParent();
+              llvm::BasicBlock* NextBB = PrevBB->splitBasicBlock(vptr);
+
+              IRB.SetInsertPoint(PrevBB, &PrevBB->back());
+              llvm::Value* CmpLowBound = IRB.CreateICmpUGE(TblPtr, IRB.CreateBitCast(BasePtr, TblPtr->getType()));
+              IRB.CreateCondBr(CmpLowBound, NextBB, ExitBB);
+
+              llvm::BasicBlock* Next2BB = NextBB->splitBasicBlock(vptr);
+
+              IRB.SetInsertPoint(NextBB, &NextBB->back());
+              llvm::Value* CmpHighBound = IRB.CreateICmpULE(TblPtr, IRB.CreateBitCast(EndPtr, TblPtr->getType()));
+              IRB.CreateCondBr(CmpHighBound, Next2BB, ExitBB);
+
+              NextBB->back().eraseFromParent();
+              PrevBB->back().eraseFromParent();
+
+
+            }
+          }
+        }
+
+        if(ExitBB->getNumUses() == 0) {
+          ExitBB->eraseFromParent();
+        }
+
       }
 
       return true;
